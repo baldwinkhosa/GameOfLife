a47431450e300ec6023d53b5c92ce01774a32f5f
baldwin <baldwinkhosa@gmail.com>
2020-11-09 07:34:04 +0200

    adding code to master

---
+++ GameOfLife/GameOfLife.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Configuration;
+using System.Text;
+using System.Threading;
+
+namespace GameOfLife
+{
+    class GameOfLife
+    {
+        private static void Print(Status[,] future, int rows, int columns, int timeout = 500)
+        {
+            var stringBuilder = new StringBuilder();
+            for (var row = 0; row < rows; row++)
+            {
+                for (var column = 0; column < columns; column++)
+                {
+                    var cell = future[row, column];
+                    stringBuilder.Append(cell == Status.Alive ? "1" : "0");
+                }
+                stringBuilder.Append("\n");
+            }
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.CursorVisible = false;
+            Console.SetCursorPosition(0, 0);
+            Console.Write(stringBuilder.ToString());
+            Thread.Sleep(timeout);
+        }
+        public static void Main()
+        {
+            int rows = 0; 
+            int columns = 0;
+            int numberOfGeneration = 0;
+            int cnt = 0;
+
+            try
+            {
+                rows = Int32.Parse(ConfigurationManager.AppSettings["Rows"]);
+                columns = Int32.Parse(ConfigurationManager.AppSettings["Columns"]);
+                numberOfGeneration = Int32.Parse(ConfigurationManager.AppSettings["GenNumber"]);
+
+                Simulation sim = new Simulation(rows, columns);
+                var grid = new Status[rows, columns];
+                var random = new Random();
+
+                for (var row = 0; row < rows; row++)
+                {
+                    for (var column = 0; column < columns; column++)
+                    {
+                        grid[row, column] = (Status) random.Next(0, 2);
+                    }
+                }
+
+                Console.CancelKeyPress += (sender, args) =>
+                {
+                    cnt = numberOfGeneration + 1;
+                    Console.WriteLine("\nEnding simulation.");
+                };
+
+                Console.Clear();
+
+                while (cnt <= numberOfGeneration)
+                {
+                    Print(grid, rows, columns);
+                    grid = sim.NextGeneration(grid);
+                    cnt++;
+                }
+               
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"An error occured {ex}");
+            }
+        }
+    }
+}
---
+++ GameOfLife/Simulation.cs
@@ -0,0 +1,56 @@
+﻿namespace GameOfLife
+{
+    public class Simulation
+    {
+        public int Rows { get; set; }
+        public int Columns { get; set; }
+
+        public Simulation(int rows, int columns)
+        {
+            Rows = rows;
+            Columns = columns;
+        }
+
+        public Status[,] NextGeneration(Status[,] currentGrid)
+        {
+            var nextGeneration = new Status[Rows, Columns];
+
+            for (var row = 1; row < Rows - 1; row++)
+                for (var column = 1; column < Columns - 1; column++)
+                {
+                    var aliveNeighbors = 0;
+                    for (var i = -1; i <= 1; i++)
+                    {
+                        for (var j = -1; j <= 1; j++)
+                        {
+                            aliveNeighbors += currentGrid[row + i, column + j] == Status.Alive ? 1 : 0;
+                        }
+                    }
+
+                    var currentCell = currentGrid[row, column];
+
+                    aliveNeighbors -= currentCell == Status.Alive ? 1 : 0;
+
+                    if (currentCell == Status.Alive && aliveNeighbors < 2)
+                    {
+                        nextGeneration[row, column] = Status.Dead;
+                    }
+ 
+                    else if (currentCell == Status.Alive && aliveNeighbors > 3)
+                    {
+                        nextGeneration[row, column] = Status.Dead;
+                    }
+
+                    else if (currentCell == Status.Dead && aliveNeighbors == 3)
+                    {
+                        nextGeneration[row, column] = Status.Alive;
+                    }
+                    else
+                    {
+                        nextGeneration[row, column] = currentCell;
+                    }
+                }
+            return nextGeneration;
+        }
+    }
+}
